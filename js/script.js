45b80e424e2eb82f2f94a125c90d94006e87bb31
sourav saiful <73190867+souravsaiful@users.noreply.github.com>
2021-01-03 15:36:00 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,35 @@
+// Js
+
+// light on off
+function on(){
+document.getElementById('light').src="images/pic_bulbon.gif";
+};
+
+function off(){
+document.getElementById('light').src="images/pic_bulboff.gif";
+};
+
+
+// light on off
+
+// font size
+function size(){
+	document.getElementById('size').style.fontSize="35px";
+}
+// font size
+
+// hide
+function hide(){
+	document.getElementById('hd').style.display="none";
+}
+// hide
+
+
+// js
+// JQ
+$(document).ready(function(){
+$("#alt").click(function(){
+	alert("hi all")
+})
+});
+// JQ
